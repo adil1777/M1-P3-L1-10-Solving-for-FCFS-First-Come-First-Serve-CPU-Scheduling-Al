2934d7acbcacb8c358f34777d174d0f4fa2a5ae6
adil1777 <97937388+adil1777@users.noreply.github.com>
2022-01-25 12:12:41 +0530

    Create 10 Solving for FCFS (First Come First Serve) .c

---
+++ M1/P3/L1/10 Solving for FCFS (First Come First Serve) .c
@@ -0,0 +1,37 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+int main() 
+{
+    int a,b,c,d;
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    cin>>d;
+
+    cout<<"P1 (WT="<<0<<"), P2 (WT="<<a<<"), P3 (WT="<<a+b<<"), P4 (WT="<<a+b+c<<")"<<endl;
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
